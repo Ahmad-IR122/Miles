1d33cbfa9bf90bdef0492285467e33f7f3f187b8
Ahmad Irshaid <145376048+AhmadIrsahaid@users.noreply.github.com>
2026-07-29 18:01:04 +0300

    Initialize Azure AI service with project structure and dependencies

---
+++ Ai-services/app/clients/azure_openai.py
@@ -0,0 +1,27 @@
+from openai import AzureOpenAI
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+
+from app.config import settings
+
+
+def get_client():
+    print("Creating Azure client...")
+    print(settings.AZURE_OPENAI_ENDPOINT)
+    print(settings.AZURE_OPENAI_API_VERSION)
+    
+    if settings.AZURE_OPENAI_API_KEY:
+        print("Using API Key")
+        return AzureOpenAI(
+            api_key=settings.AZURE_OPENAI_API_KEY,
+            azure_endpoint=settings.AZURE_OPENAI_ENDPOINT,
+            api_version="2025-03-01-preview",
+        )
+    token_provider = get_bearer_token_provider(
+      DefaultAzureCredential(),
+      "https://cognitiveservices.azure.com/.default",
+    )
+    return AzureOpenAI(
+        azure_ad_token_provider=token_provider,
+        azure_endpoint=settings.AZURE_OPENAI_ENDPOINT,
+        api_version=settings.AZURE_OPENAI_API_VERSION,
+    )
---
+++ Ai-services/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from app.services.chat import ask
+
+app = FastAPI()
+
+class PromptRequest(BaseModel):
+  prompt: str
+
+@app.get("/")
+def read_root():
+  return {"message": "Hello, World!" } # for testing purposes, you can remove this later
+
+@app.post("/chat")
+def chat(request: PromptRequest):
+  response = ask(request.prompt)
+  return {"response": response}
