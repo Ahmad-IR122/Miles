8b5ab4f79e7cbdebde07403d3fec1a59093ac4fe
Ahmad Irshaid <145376048+AhmadIrsahaid@users.noreply.github.com>
2026-08-05 00:47:08 +0300

    remove moke data

--- backend/app/routers/itinerary.py
+++ backend/app/routers/itinerary.py
@@ -2,50 +2,7 @@ from fastapi import APIRouter
 
 router = APIRouter(prefix="/itinerary", tags=["itinerary"])
 
-itinerary_data = [
-    {
-        "destination": "Rome",
-        "startDate": "2026-08-10",
-        "endDate": "2026-08-15",
-        "days": [
-            {
-                "day": 1,
-                "activities": [
-                    "Visit the Colosseum",
-                    "Lunch at a local restaurant",
-                ],
-            },
-            {
-                "day": 2,
-                "activities": [
-                    "Visit the Vatican",
-                    "Walk around Piazza Navona",
-                ],
-            },
-        ],
-    },
-    {
-        "destination": "Nablus",
-        "startDate": "2026-08-10",
-        "endDate": "2026-08-15",
-        "days": [
-            {
-                "day": 1,
-                "activities": [
-                    "Visit the old city",
-                    "Lunch at a W restaurant",
-                ],
-            },
-            {
-                "day": 2,
-                "activities": [
-                    "Visit the Vatican",
-                    "Walk around Piazza Navona",
-                ],
-            },
-        ],
-    },
-]# use a database to store itinerary data in a real application
+itinerary_data = [ ]# use a database to store itinerary data in a real application
 @router.get("")
 def health_check_itinerary():
     return {"data": itinerary_data}
