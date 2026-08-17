9fe9e4ace27dd35789abcfc24392476e22042fae
Hamza Droubi (Trainee) <t-hamza.droubi@asaltech.com>
2026-08-17 11:16:25 +0000

    Merged PR 361: route protection

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -7,6 +7,7 @@ import PlanTrip from "./plan-trip";
 import SignUpPage from "../features/auth/pages/signUp";
 import SignInPage from "../features/auth/pages/signIn";
 import Recommendations from "../features/recommendations/pages/recommendations";
+import ProtectedRoute from "../features/auth/components/protectedRoute";
 import { routesPaths } from "./routesPaths";
 
 const router = createBrowserRouter([
@@ -19,7 +20,11 @@ const router = createBrowserRouter([
       },
       {
         path: routesPaths.planTrip,
-        element: <PlanTrip />,
+        element: (
+          <ProtectedRoute>
+            <PlanTrip />
+          </ProtectedRoute>
+        ),
       },
       {
         path: routesPaths.recommendation,
@@ -27,11 +32,19 @@ const router = createBrowserRouter([
       },
       {
         path: routesPaths.itinerary,
-        element: <Itinerary />,
+        element: (
+          <ProtectedRoute>
+            <Itinerary />
+          </ProtectedRoute>
+        ),
       },
       {
         path: routesPaths.chatbot,
-        element: <ChatPage />,
+        element: (
+          <ProtectedRoute>
+            <ChatPage />
+          </ProtectedRoute>
+        ),
       },
       {
         path: routesPaths.signUp,
