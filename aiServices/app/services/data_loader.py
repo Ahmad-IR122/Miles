530b6631a0280956ca11c7b33a38fd547ce068d7
Ahmad Irshaid <145376048+AhmadIrsahaid@users.noreply.github.com>
2026-08-22 20:45:06 +0300

    Implement recommendation data loading , update recommendation logic and load dataset from azure blob storage

---
+++ aiServices/app/services/data_loader.py
@@ -0,0 +1,41 @@
+import os
+from io import BytesIO
+
+import pandas as pd
+from azure.storage.blob import BlobServiceClient
+from azure.identity import DefaultAzureCredential
+
+ACCOUNT_URL = os.getenv("AZURE_STORAGE_ACCOUNT_URL")
+CONTAINER_NAME = os.getenv("AZURE_STORAGE_CONTAINER")
+
+credential = DefaultAzureCredential()
+blob_service_client = BlobServiceClient(account_url=ACCOUNT_URL, credential=credential)
+
+def load_csv(blob_name : str) -> pd.DataFrame:
+    """
+    Load a CSV file from Azure Blob Storage and return it as a pandas DataFrame.
+
+    Args:
+        blob_name (str): The name of the blob (CSV file) to load.
+
+    Returns:
+        pd.DataFrame: The loaded data as a pandas DataFrame.
+    """
+    
+    blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=blob_name)
+    data = blob_client.download_blob().readall()
+    
+    return pd.read_csv(BytesIO(data))
+  
+
+def load_recommendation_data():
+    """
+    Load the recommendation data from Azure Blob Storage.
+
+    Returns:
+        tuple: A tuple containing two pandas DataFrames: destinations and activities.
+    """
+    destinations = load_csv("destinations.csv")
+    activities = load_csv("activities.csv")
+    restaurants = load_csv("restaurants.csv")
+    return destinations, activities, restaurants
--- aiServices/app/services/recommendation_service.py
+++ aiServices/app/services/recommendation_service.py
@@ -1,11 +1,7 @@
+from app.services.data_loader import load_recommendation_data
+
+
+destinations, activities, restaurants = load_recommendation_data()
 def recommend_destinations(user_preferences):
-    """
-    Recommend travel destinations based on user preferences.
+  pass
 
-    Args:
-        user_preferences (dict): A dictionary containing user preferences such as
-        preferred climate, activities, budget, etc.
-
-    Returns:
-        list: A list of recommended destinations.
-    """
