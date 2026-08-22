0bd90adbe864fccb33feab15f441f0aaceee4277
Ahmad Irshaid <145376048+AhmadIrsahaid@users.noreply.github.com>
2026-08-22 13:08:39 +0300

    init file of recommendation_service

---
+++ aiServices/app/services/recommendation_service.py
@@ -0,0 +1,11 @@
+def recommend_destinations(user_preferences):
+    """
+    Recommend travel destinations based on user preferences.
+
+    Args:
+        user_preferences (dict): A dictionary containing user preferences such as
+        preferred climate, activities, budget, etc.
+
+    Returns:
+        list: A list of recommended destinations.
+    """
