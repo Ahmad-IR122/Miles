83784ff8bd4acc58794ed38516ac2f4ab569854d
Ahmad Irshaid (Trainee) <t-ahmad.irshaid@asaltech.com>
2026-08-23 09:02:27 +0000

    Merged PR 483: fix issues of configuration of FE env files
    this PR was created to fix the PR [Pull Request 440: configuration for FE environment variables](https://dev.azure.com/AI26s/Project%20A/_git/Project%20A/pullrequest/440)  
    
    Related work items: #1666

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -1,8 +1,7 @@
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ?? "http://127.0.0.1:8000/";
+const API_BASE_URL = import.meta.env.VITE_API_URL ?? "http://127.0.0.1:8000/";
 
 if (!API_BASE_URL) {
-  throw new Error("VITE_API_BASE_URL is not configured");
+  throw new Error("VITE_API_URL is not configured");
 }
 
 export { API_BASE_URL };
